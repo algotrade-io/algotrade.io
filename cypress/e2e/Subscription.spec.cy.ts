cc590b76f869b9d2e3d830fa88b821c0c4eecbcc
Krish Suchak <suchak.krish@gmail.com>
2025-06-10 01:30:20 -0400

    increase timeout

--- cypress/e2e/Subscription.spec.cy.ts
+++ cypress/e2e/Subscription.spec.cy.ts
@@ -32,7 +32,7 @@ describe('Subscription', () => {
           cy.get('input[type="checkbox"]').uncheck()
           cy.wait(5000)
           cy.get('.SubmitButton').contains('span', 'Subscribe').click({force: true});
-          cy.wait(20000)
+          cy.wait(30000)
         });
 
         // Cancel subscription through Billing page
