d41f0eb9a6f4b29e0936c3da40a779f6f5e585d1
Krish Suchak <suchak.krish@gmail.com>
2026-02-06 05:20:21 -0500

    all caps

--- src/api/test/signals/test_signals.py
+++ src/api/test/signals/test_signals.py
@@ -45,7 +45,7 @@ def test_get_signals():
         assert datum["Signal"] == "BUY" or datum["Signal"] == "SELL"
         assert datum["Day"] in {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"}
         assert datum["Asset"] == "BTC"
-    for _ in range(max_accesses):
+    for _ in range(MAX_ACCESSES):
         remaining = update_access_queue(user)
 
     assert not remaining
